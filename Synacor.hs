12ea190464c62316d7fcb2839ca1fa737af99ad1
Johan Liesén <johan@liesen.se>
2019-03-29 00:31:01 +0100

    read/write

--- Synacor.hs
+++ Synacor.hs
@@ -65,108 +65,109 @@ step m@Machine{..} =
     debugShow m $ case op of
         -- halt: 0
         --   stop execution and terminate the program
-        Halt -> return $ debug "halt" Nothing
+        Halt -> debugOp [] $ return Nothing
         -- set: 1 a b
         --   set register <a> to the value of <b>
-        Set ->
+        Set -> do
             let [a, b] = take 2 args
-            in return $ debug (intercalate " " ["set", show a, show b]) $
-                Just (m{reg = reg // [(r a, v b)], pc = pc + 3})
+            debugOp [a, b] $
+                return $ Just (write a (v b)) { pc = pc + 3 }
         -- push: 2 a
         --   push <a> onto the stack
-        Push ->
+        Push -> do
             let [a] = take 1 args
-            in return $ debug (intercalate " " ["push", show a]) $ Just (m{stack = v a:stack, pc = pc + 2})
+            debugOp [a] $
+                return $ Just (m{stack = v a:stack, pc = pc + 2})
         -- pop: 3 a
         --   remove the top element from the stack and write it into <a>; empty stack = error
         Pop -> do
             let [a] = take 1 args
                 s:ss = stack
-            debugOp [a] $ return $ Just (m{stack = ss, reg = reg // [(r a, s)], pc = pc + 2})
+            debugOp [a] $
+                return $ Just (write a s) { stack = ss, pc = pc + 2 }
         -- eq: 4 a b c
         --   set <a> to 1 if <b> is equal to <c>; set it to 0 otherwise
         Eq -> do
             let [a, b, c] = take 3 args
             debugOp [a, b, c] $
-                return $ Just (m{reg = reg // [(r a, if v b == v c then 1 else 0)], pc = pc + 4})
+                return $ Just (write a (if v b == v c then 1 else 0)) { pc = pc + 4 }
         -- gt: 5 a b c
         --   set <a> to 1 if <b> is greater than <c>; set it to 0 otherwise
         Gt -> do
             let [a, b, c] = take 3 args
             debugOp [a, b, c] $
-                return $ Just (m{reg = reg // [(r a, if v b > v c then 1 else 0)], pc = pc + 4})
+                return $ Just (write a (if v b > v c then 1 else 0)) { pc = pc + 4 }
         -- jmp: 6 a
         --   jump to <a>
         Jmp -> do
             let [a] = take 1 args
-            debugOp [a] $ return $ Just (m{pc = v a})
+            debugOp [a] $
+                return $ Just (m{pc = v a})
         -- jt: 7 a b
         --   if <a> is nonzero, jump to <b>
         Jt -> do
             let [a, b] = take 2 args
-            debugOp [a, b] $ return $ Just (m{pc = if v a /= 0 then v b else pc + 3})
+            debugOp [a, b] $
+                return $ Just (m{pc = if v a /= 0 then v b else pc + 3})
         -- jf: 8 a b
         --   if <a> is zero, jump to <b>
         Jf -> do
             let [a, b] = take 2 args
-            debugOp [a, b] $ return $ Just (m{pc = if v a == 0 then v b else pc + 3})
+            debugOp [a, b] $
+                return $ Just (m{pc = if v a == 0 then v b else pc + 3})
         -- add: 9 a b c
         --   assign into <a> the sum of <b> and <c> (modulo 32768)
         Add -> do
             let [a, b, c] = take 3 args
             debugOp [a, b, c] $
-                return $ Just (m{reg = reg // [(r a, (v b + v c) `mod` 32768)], pc = pc + 4})
+                return $ Just (write a ((v b + v c) `mod` 32768)) { pc = pc + 4 }
         -- mult: 10 a b c
         --   store into <a> the product of <b> and <c> (modulo 32768)
         Mult -> do
             let [a, b, c] = take 3 args
             debugOp [a, b, c] $
-                return $ Just (m{reg = reg // [(r a, (v b * v c) `mod` 32768)], pc = pc + 4})
+                return $ Just (write a ((v b * v c) `mod` 32768)) { pc = pc + 4 }
         -- mod: 11 a b c
         --   store into <a> the remainder of <b> divided by <c>
         Mod -> do
             let [a, b, c] = take 3 args
             debugOp [a, b, c] $
-                return $ Just (m{reg = reg // [(r a, v b `mod` v c)], pc = pc + 4})
+                return $ Just (write a (v b `mod` v c)) { pc = pc + 4 }
         -- and: 12 a b c
         --   stores into <a> the bitwise and of <b> and <c>
         And -> do
             let [a, b, c] = take 3 args
             debugOp [a, b, c] $
-                return $ Just (m{reg = reg // [(r a, v b .&. v c)], pc = pc + 4})
+                return $ Just (write a (v b .&. v c)) { pc = pc + 4 }
         -- or: 13 a b c
         --   stores into <a> the bitwise or of <b> and <c>
         Or -> do
             let [a, b, c] = take 3 args
             debugOp [a, b, c] $
-                return $ Just (m{reg = reg // [(r a, v b .|. v c)], pc = pc + 4})
+                return $ Just (write a (v b .|. v c)) { pc = pc + 4 }
         -- not: 14 a b
         --   stores 15-bit bitwise inverse of <b> in <a>
         Not -> do
             let [a, b] = take 2 args
             debugOp [a, b] $
-                return $ Just (m{reg = reg // [(r a, complement (v b) .&. 32767)], pc = pc + 3})
+                return $ Just (write a (complement (v b) `mod` 32768)) {pc = pc + 3}
         -- rmem: 15 a b
         --   read memory at address <b> and write it to <a>
         Rmem -> do
             let [a, b] = take 2 args
-            debugOp [a, b] $
-                return $ case (x a, v b) of
-                    (Reg a, b) -> Just (m{reg = reg // [(a, mem ! b)], pc = pc + 3})
+            debugOp [a, b] $ return $ Just (write a (read (v b))) { pc = pc + 3 }
         -- wmem: 16 a b
         --   write the value from <b> into memory at address <a>
         Wmem -> do
             let [a, b] = take 2 args
             debugOp [a, b] $
-                -- return $ Just $ (write (v a) (v b)){pc = pc + 3}
-                return $ case (x (v a), v b) of
-                    (Reg a, b) -> Just (m{reg = reg // [(a, b)], pc = pc + 3})
-                    (Lit a, b) -> Just (m{mem = mem // [(a, b)], pc = pc + 3})
+                return $ Just (write (v a) (v b)) { pc = pc + 3 }
         -- call: 17 a
         --   write the address of the next instruction to the stack and jump to <a>
         Call -> do
             let [a] = take 1 args
-            debugOp [a] $ return $ Just (m{stack = (pc + 2):stack, pc = v a})
+            debugOp [a] $
+                return $ Just (m{stack = (pc + 2):stack, pc = v a})
         -- ret: 18
         --   remove the top element from the stack and jump to it; empty stack = halt
         Ret -> debugOp [] $ return $ case stack of
@@ -183,9 +184,8 @@ step m@Machine{..} =
         In -> do
             let [a] = take 1 args
             b <- getChar
-            debugOp [a] $ return $
-                case x a of
-                    Reg a -> Just (m{reg = reg // [(a, fromIntegral (ord b))], pc = pc + 2})
+            debugOp [a] $
+                return $ Just (write a (fromIntegral (ord b))) { pc = pc + 2 }
         Noop -> debugOp [] $ return $ Just (m{pc = pc + 1})
     where
         opcode = mem ! pc
@@ -212,7 +212,7 @@ step m@Machine{..} =
             | otherwise = error "numbers 32776..65535 are invalid"
 
         read a | a < 32768 = mem ! a
-               | a < 32776 = mem ! (reg ! (a - 32768))
+               | a < 32776 = reg ! (a - 32768)
                | otherwise = error "numbers 32776..65535 are invalid"
 
         debug :: String -> a -> a
